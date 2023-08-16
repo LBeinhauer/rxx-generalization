7b5a47b6198c841dbb0f707915f4b4503db4bf10
LBeinhauer <48241061+LBeinhauer@users.noreply.github.com>
2023-08-16 18:37:51 +0200

    ENABLE BACK-TRANSFORMATION

--- Prediction_Heterogeneity_in_Bonett-estimates.R
+++ Prediction_Heterogeneity_in_Bonett-estimates.R
@@ -1,4 +1,6 @@
 
+library(here)
+
 DF <- read.csv(here("Notes/vis_df.csv"), sep = "")
 
 
@@ -17,6 +19,7 @@ rma_Bonett <- lapply(1:length(Large_Sim_Data), FUN = function(it){
                           vi = var)
   
   return(list(tau2 = rma_Bon$tau2,
+              mu = rma_Bon$b[1],
               H2 = rma_Bon$H2,
               k = rma_Bon$k,
               QE = rma_Bon$QE,
@@ -33,6 +36,8 @@ H2 <- unlist(lapply(rma_Bonett, FUN = function(x){x$H2}))
 k <- unlist(lapply(rma_Bonett, FUN = function(x){x$k}))
 Q <- unlist(lapply(rma_Bonett, FUN = function(x){x$Q}))
 I2 <- unlist(lapply(rma_Bonett, FUN = function(x){x$I2}))
+mu <- unlist(lapply(rma_Bonett, FUN = function(x){x$mu}))
+
 
 (Q+(k-1))/(k-1)
 
@@ -56,7 +61,9 @@ tau_varX <- sqrt(tau_varT^2 + tau_varE^2)
 
 cov_varT_varX <- tau_varT^2 - (1/2 * tau_varE^2)
 
-pred.tau_rel <- (tau_varT/(mu_varX^2)) + ((mu_varT^2 * tau_varX)/mu_varX^4) + ((2*mu_varT*(tau_varT^2 - .5*(tau_varE^2)))/mu_varX^3)
+pred.tau_rel <- ((tau_varT^2)/(mu_varX^2)) + (((mu_varT^2) * (tau_varX^2))/(mu_varX^4)) - ((2*mu_varT*(tau_varT^2))/(mu_varX^3))
+
+pred.tau_rel2 <- ((tau_varT^2)/(mu_varX^2)) + (((mu_varT^2)/(mu_varX^4))*(tau_varX^2)) - (((2*mu_varT)/(mu_varX^3))*(tau_varT^2)) 
 
 
 tau_lnvarT <- sqrt(log(((tau_varT^2) / (mu_varT^2)) + 1))
@@ -73,6 +80,15 @@ pred.tau2_Bonett2 <- tau_lnvarE^2 +
   2*((tau_varE^2/((mu_varE)*(mu_varX))) +
        (((tau_varE^2) + (tau_varE*tau_varT))/(4*(mu_varE^2)*(mu_varX^2))))
   
+
+tau2 <- unlist(tau2_Bonett)
+
+back.transform_tau2 <- ((exp(mu)^2)*tau2) + (((exp(mu)^2) / 2) * (tau2^2))
+
+
+
+pred.mu_untransformed <- (mu_varT/mu_varX) + ((tau_varX^2)*(mu_varT/(mu_varX^3))) - ((tau_varT^2)/(mu_varX^2))
+
 
 
 
@@ -96,12 +112,25 @@ pred.H2_inconsistent <- pred.Q / 99
 pred.I2 <- 100 * (pred.tau2_Bonett2 / (pred.tau2_Bonett2 + pred.nu))
 
 
+
+
+
+
+
 library(ggplot2)
 
 ggplot() +
-  geom_point(aes(x = pred.tau_rel, y = DF$tau_rel), alpha = .1,
+  geom_point(aes(x = sqrt(pred.tau_rel), y = DF$tau_rel), alpha = .1,
              position = position_jitter(width = .001)) +
   geom_abline(intercept = 0, slope = 1)
+
+
+ggplot() +
+  geom_point(aes(x = sqrt(pred.tau_rel), y = sqrt(back.transform_tau2)), alpha = .1,
+             position = position_jitter(width = .001)) +
+  geom_abline(intercept = 0, slope = 1)
+
+
 
 
 ggplot() +
@@ -147,7 +176,64 @@ dftest2 <- data.frame(pred = pred.tau2_Bonett2,
                       H2_consistent = H2,
                       I2_consistent = I2,
                       H2_inconsistent = pred.H2_inconsistent,
-                      I2 = pred.I2)
+                      I2 = pred.I2,
+                      tau_back = sqrt(back.transform_tau2),
+                      tau_untr_pred = sqrt(pred.tau_rel),
+                      tau_untr_pred2 = sqrt(pred.tau_rel2))
+
+
+ggplot(data = dftest2) +
+  #geom_abline(intercept = 0, slope = 1) + 
+  geom_point(aes(x = rel, y = tau_back, colour = as.factor(rel)), alpha = .5,
+             position = position_jitter(width = .01)) +
+  facet_grid(rows = vars(CVT),
+             cols = vars(CVE))
+
+ggplot(data = dftest2) +
+  #geom_abline(intercept = 0, slope = 1) + 
+  geom_point(aes(x = rel, y = tau_untr_pred, colour = as.factor(rel)), alpha = .5,
+             position = position_jitter(width = .01)) +
+  facet_grid(rows = vars(CVT),
+             cols = vars(CVE))
+
+
+ggplot(data = dftest2) + 
+  geom_abline(intercept = 0, slope = 1) + 
+  geom_point(aes(x = tau_untr_pred, y = tau_back, colour = as.factor(rel)), alpha = .5,
+             position = position_jitter(width = .01)) +
+  facet_grid(rows = vars(CVT),
+             cols = vars(CVE))
+
+
+
+
+## with 2nd-order Taylor Polynomial approximation
+# 
+# ggplot(data = dftest2) +
+#   #geom_abline(intercept = 0, slope = 1) + 
+#   geom_point(aes(x = rel, y = tau_untr_pred2, colour = as.factor(rel)), alpha = .5) +
+#   facet_grid(rows = vars(CVT),
+#              cols = vars(CVE))
+# 
+# 
+# ggplot(data = dftest2) + 
+#   geom_abline(intercept = 0, slope = 1) + 
+#   geom_point(aes(x = tau_untr_pred2, y = tau_back, colour = as.factor(rel)), alpha = .5) +
+#   facet_grid(rows = vars(CVT),
+#              cols = vars(CVE))
+# 
+
+
+ggplot(data = dftest2) + 
+  geom_abline(intercept = 0, slope = 1) + 
+  geom_point(aes(x = tau_untr_pred, y = tau_back, colour = as.factor(CVT)), alpha = .5) +
+  facet_grid(rows = vars(CVE),
+             cols = vars(rel))
+
+
+
+
+
 
 ggplot(data = dftest2) +
   geom_abline(intercept = 0, slope = 1) + 
@@ -177,6 +263,14 @@ ggplot(data = dftest2) +
   geom_point(aes(x = sqrt(pred), y = H2_consistent, colour = as.factor(rel)), alpha = .5,
              position = position_jitter(width = .01)) + 
   geom_line(aes(x = sqrt(pred), y = pred.H2), linewidth = 2) + 
+  facet_grid(rows = vars(CVT),
+             cols = vars(CVE))
+
+
+
+ggplot(data = dftest2) +
+  geom_point(aes(x = H2, y = H2_consistent, colour = as.factor(rel)), alpha = .5) + 
+  geom_abline(intercept = 0, slope = 1) + 
   facet_grid(rows = vars(CVT),
              cols = vars(CVE))
 
